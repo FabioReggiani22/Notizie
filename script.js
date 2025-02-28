0a82aa6a5f6997f6db57b73cfc32ab9606047323
FabioReggiani22 <fabioreg22@gmail.com>
2025-02-28 23:36:54 +0100

    Prova

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Benvenuto nel prototipo del sito!");
+let slideIndex = 0;
+
+function moveSlide(step) {
+    const slides = document.querySelectorAll('.notizia');
+    const totalSlides = slides.length;
+    
+    // Calcolare il nuovo indice
+    slideIndex = (slideIndex + step + totalSlides) % totalSlides;
+    
+    // Aggiornare la posizione del carosello
+    const carousel = document.querySelector('.carousel');
+    carousel.style.transform = `translateX(-${slideIndex * 100}%)`;
+}
